41406e09bb48cbeb979c4a901998a5f49d71e62b
Rhyker Wells <a.rhykerw@gmail.com>
2026-03-24 12:31:59 +0000

    refactor: rewrite middleware parsing to be more scaleable

--- web/middleware.go
+++ web/middleware.go
@@ -4,15 +4,12 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/base64"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"net/url"
 	"reflect"
-	"sort"
-	"strconv"
 	"time"
 
 	"github.com/RhykerWells/Summit/bot/functions"
@@ -31,6 +28,11 @@ type CtxKey int
 
 const (
 	CtxKeyTmplData CtxKey = iota
+	CtxKeyCurrentUser
+	CtxKeyOAuthToken
+	CtxKeyUserManagedGuilds
+	CtxKeyAvailableGuilds
+	CtxKeyCurrentGuild
 	CtxKeyFormParsed
 )
 
@@ -120,133 +122,6 @@ func deleteCookie(w http.ResponseWriter, cookie *http.Cookie) {
 	http.SetCookie(w, cookie)
 }
 
-// getUserGuilds returns the guild IDs that the user can currently manage
-// and the guild IDs that the user has the correct permission to manage if the bot is added
-func getUserManagedGuilds(w http.ResponseWriter, r *http.Request, ctx context.Context, token *oauth2.Token) (map[string]string, map[string]string) {
-	user := tokenToUser(w, r, ctx, token)
-
-	managedGuilds := make(map[string]string)
-	for _, guild := range common.Session.State.Guilds {
-		member, err := common.Session.GuildMember(guild.ID, user.ID)
-		if err != nil {
-			continue
-		}
-		managed := isUserManaged(guild.ID, member)
-		if managed {
-			// Store the guild ID and name in the map
-			managedGuilds[guild.ID] = guild.Name
-		}
-	}
-
-	client := OauthConf.Client(ctx, token)
-	resp, err := client.Get("https://discord.com/api/v10/users/@me/guilds")
-	if err != nil {
-	}
-
-	if resp.StatusCode != http.StatusOK {
-	}
-	defer resp.Body.Close()
-
-	type PartialGuild struct {
-		ID          string `json:"id"`
-		Name        string `json:"name"`
-		Permissions string `json:"permissions"`
-	}
-	var guilds []PartialGuild
-	json.NewDecoder(resp.Body).Decode(&guilds)
-
-	availableGuilds := make(map[string]string)
-	for _, guild := range guilds {
-		if _, ok := managedGuilds[guild.ID]; ok {
-			continue
-		}
-
-		permInt, _ := strconv.Atoi(guild.Permissions)
-		var requiredPerms = discordgo.PermissionManageServer | discordgo.PermissionAdministrator
-		if permInt&requiredPerms != 0 {
-			availableGuilds[guild.ID] = guild.Name
-		}
-	}
-
-	return managedGuilds, availableGuilds
-}
-
-// isUserManaged returns a boolean of whether or not the user has the permissions to manage the guild
-// Permissions required are: Owner, Manage Server or Administrator
-func isUserManaged(guildID string, member *discordgo.Member) bool {
-	guild, err := common.Session.Guild(guildID)
-	if err == nil && guild.OwnerID == member.User.ID {
-		return true
-	}
-
-	for _, roleID := range member.Roles {
-		role, err := common.Session.State.Role(guildID, roleID)
-		if err != nil {
-			continue
-		}
-
-		if (role.Permissions&discordgo.PermissionAdministrator != 0) ||
-			(role.Permissions&discordgo.PermissionManageServer != 0) {
-			return true
-		}
-	}
-
-	return false
-}
-
-// validateGuild ensures users can't access the manage page for guilds without the correct permissions
-func validateGuild(inner http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		guildIDStr := pat.Param(r, "server")
-		_, err := strconv.ParseInt(guildIDStr, 10, 64)
-		if err != nil {
-			http.Redirect(w, r, "/?error=invalid_guild", http.StatusFound)
-			return
-		}
-
-		token, err := checkUserCookie(w, r)
-		if err != nil {
-			http.Redirect(w, r, "/?error=no_access", http.StatusFound)
-			return
-		}
-		user := tokenToUser(w, r, r.Context(), token)
-
-		member, err := functions.GetMember(guildIDStr, user.ID)
-		if err != nil {
-			http.Redirect(w, r, "/?error=no_access", http.StatusFound)
-			return
-		}
-
-		managed := isUserManaged(guildIDStr, member)
-		if !managed {
-			http.Redirect(w, r, "/?error=no_access", http.StatusFound)
-			return
-		}
-
-		inner.ServeHTTP(w, r)
-	})
-}
-
-type TmplContextData map[string]interface{}
-
-// baseTemplateDataMW provides the initial template data to be parsed within each page
-func baseTemplateDataMW(inner http.Handler) http.Handler {
-	middleware := func(w http.ResponseWriter, r *http.Request) {
-		baseData := TmplContextData{
-			"HomeURL": URL,
-			"Year":    time.Now().UTC().Year(),
-			"Path":    r.URL.Path,
-			"Version": common.VERSION,
-			"Testing": common.ConfigTestMode,
-		}
-		ctx := context.WithValue(r.Context(), CtxKeyTmplData, baseData)
-
-		inner.ServeHTTP(w, r.WithContext(ctx))
-	}
-
-	return http.HandlerFunc(middleware)
-}
-
 type GithubRelease struct {
 	HTMLURL     string    `json:"html_url"`
 	Name        string    `json:"name"`
@@ -256,9 +131,24 @@ type GithubRelease struct {
 	BodyHTML    template.HTML
 }
 
-// userAndManagedGuildsInfoMW provides middleware to parse the current user data and the list of manageable guilds to the template data
-func userAndManagedGuildsInfoMW(inner http.Handler) http.Handler {
-	middleware := func(w http.ResponseWriter, r *http.Request) {
+type TmplContextData map[string]interface{}
+
+func BaseTemplateDataMW(inner http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		baseData := TmplContextData{
+			"HomeURL": URL,
+			"Year":    time.Now().UTC().Year(),
+			"Path":    r.URL.Path,
+			"Version": common.VERSION,
+			"Testing": common.ConfigTestMode,
+		}
+
+		inner.ServeHTTP(w, r.WithContext(SetTmplContextData(r.Context(), baseData)))
+	})
+}
+
+func CurrentUserMW(inner http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 
 		token, err := checkUserCookie(w, r)
@@ -266,70 +156,98 @@ func userAndManagedGuildsInfoMW(inner http.Handler) http.Handler {
 			inner.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
-		user := tokenToUser(w, r, ctx, token)
 
-		managedGuilds, availableGuilds := getUserManagedGuilds(w, r, ctx, token)
-		// Remove managed guilds from available list
-		for id := range managedGuilds {
-			delete(availableGuilds, id)
+		user, err := tokenToUser(ctx, token)
+		if err != nil {
+			inner.ServeHTTP(w, r.WithContext(ctx))
+			return
 		}
 
-		fullManagedGuilds := getPopulatedGuildList(managedGuilds, URL+"/static/img/icons/question.svg", true)
-		fullAvailableGuilds := getPopulatedGuildList(availableGuilds, URL+"/static/img/icons/plus.svg", false)
+		managedGuilds, availableGuilds, err := getUserManagedGuilds(ctx, token)
+		if err != nil {
+			inner.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
 
-		tmplData, _ := ctx.Value(CtxKeyTmplData).(TmplContextData)
-		tmplData["User"] = user
-		tmplData["ManagedGuilds"] = fullManagedGuilds
-		tmplData["AvailableGuilds"] = fullAvailableGuilds
+		ctx = context.WithValue(ctx, CtxKeyCurrentUser, user)
+		ctx = context.WithValue(ctx, CtxKeyOAuthToken, token)
+		ctx = context.WithValue(ctx, CtxKeyUserManagedGuilds, managedGuilds)
+		ctx = context.WithValue(ctx, CtxKeyAvailableGuilds, availableGuilds)
 
-		ctx = context.WithValue(ctx, CtxKeyTmplData, tmplData)
-		inner.ServeHTTP(w, r.WithContext(ctx))
-	}
+		tmplData := TmplContextData{
+			"User":            user,
+			"ManagedGuilds":   managedGuilds,
+			"AvailableGuilds": availableGuilds,
+		}
 
-	return http.HandlerFunc(middleware)
+		inner.ServeHTTP(w, r.WithContext(SetTmplContextData(ctx, tmplData)))
+	})
 }
 
-// currentGuildDataMW provides middleware to parse the current guilds data to the template data
-func currentGuildDataMW(inner http.Handler) http.Handler {
-	middleware := func(w http.ResponseWriter, r *http.Request) {
+func RequireUserMW(inner http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		user, ok := ctx.Value(CtxKeyCurrentUser).(*discordgo.User)
+		if !ok || user == nil {
+			http.Redirect(w, r, "/?error=no_access", http.StatusForbidden)
+			return
+		}
+
+		inner.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func CurrentGuildMW(inner http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		guildID := pat.Param(r, "server")
 
-		retrievedGuild, _ := common.Session.Guild(guildID)
-
-		channels, _ := common.Session.GuildChannels(guildID)
-		sort.SliceStable(channels, func(i, j int) bool {
-			return channels[i].Position < channels[j].Position
-		})
-
-		roles := retrievedGuild.Roles
-		sort.SliceStable(roles, func(i, j int) bool {
-			return roles[i].Position > roles[j].Position
-		})
-
-		member, _ := functions.GetMember(retrievedGuild.ID, common.Bot.ID)
-		role := functions.HighestRole(retrievedGuild.ID, member)
-
-		guildData := map[string]interface{}{
-			"ID":                     retrievedGuild.ID,
-			"Name":                   retrievedGuild.Name,
-			"Avatar":                 retrievedGuild.IconURL("1024"),
-			"Channels":               channels,
-			"Roles":                  roles,
-			"BotHighestRolePosition": role.Position,
-		}
-		if guildData["Avatar"] == "" {
-			guildData["Avatar"] = URL + "/static/img/icons/question.svg"
+		guild, err := common.Session.Guild(guildID)
+		if err != nil {
+			http.Redirect(w, r, "/?error=invalid_guild", http.StatusNotFound)
+			return
 		}
 
-		tmplData, _ := ctx.Value(CtxKeyTmplData).(TmplContextData)
-		tmplData["CurrentGuild"] = guildData
+		user, ok := ctx.Value(CtxKeyCurrentUser).(*discordgo.User)
+		if !ok || user == nil {
+			http.Redirect(w, r, "/?error=no_access", http.StatusForbidden)
+			return
+		}
 
-		ctx = context.WithValue(ctx, CtxKeyTmplData, tmplData)
+		member, err := functions.GetMember(guildID, user.ID)
+		if err != nil {
+			http.Redirect(w, r, "/?error=no_access", http.StatusForbidden)
+			return
+		}
+
+		if !isUserManaged(guildID, member) {
+			http.Redirect(w, r, "/?error=no_access", http.StatusForbidden)
+			return
+		}
+
+		tmplData := TmplContextData{
+			"CurrentGuild": getGuildTmplData(guild),
+		}
+
+		ctx = context.WithValue(ctx, CtxKeyCurrentGuild, guild)
+
+		inner.ServeHTTP(w, r.WithContext(SetTmplContextData(ctx, tmplData)))
+	})
+}
+
+func RequireGuildMW(inner http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		guild, ok := ctx.Value(CtxKeyCurrentGuild).(*discordgo.Guild)
+		if !ok || guild == nil {
+			http.Redirect(w, r, "/?error=no_access", http.StatusForbidden)
+			return
+		}
+
 		inner.ServeHTTP(w, r.WithContext(ctx))
-	}
-
-	return http.HandlerFunc(middleware)
+	})
 }
 
 // urlDataMW provides middleware to parse the URL data to the template data
@@ -358,53 +276,6 @@ func urlDataMW(inner http.Handler) http.Handler {
 	}
 
 	return http.HandlerFunc(middleware)
-}
-
-// getPopulatedGuildList returns a list of guilds with their ID, name and avatar URL to be used for template data
-func getPopulatedGuildList(guilds map[string]string, defaultIcon string, useGuildIcon bool) []map[string]interface{} {
-	guildList := make([]map[string]interface{}, 0)
-	for guildID, guildName := range guilds {
-		avatarURL := defaultIcon
-		if useGuildIcon {
-			if guild, err := common.Session.Guild(guildID); err == nil {
-				if url := guild.IconURL("1024"); url != "" {
-					avatarURL = url
-				}
-			}
-		}
-
-		guildList = append(guildList, TmplContextData{
-			"ID":     guildID,
-			"Avatar": avatarURL,
-			"Name":   guildName,
-		})
-	}
-
-	return guildList
-}
-
-// tokenToUser converts an OAuth token to a valid discordgo.User object
-func tokenToUser(w http.ResponseWriter, r *http.Request, ctx context.Context, token *oauth2.Token) *discordgo.User {
-	client := OauthConf.Client(ctx, token)
-	resp, err := client.Get("https://discord.com/api/v10/users/@me")
-	if err != nil {
-		http.Redirect(w, r, "/?error=failed_retrieving_info", http.StatusTemporaryRedirect)
-		return nil
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		http.Redirect(w, r, "/?error=discord_api_error", http.StatusTemporaryRedirect)
-		return nil
-	}
-	defer resp.Body.Close()
-
-	var user *discordgo.User
-	if err := json.NewDecoder(resp.Body).Decode(&user); err != nil {
-		http.Redirect(w, r, "/?error=json_decode_error", http.StatusTemporaryRedirect)
-		return nil
-	}
-
-	return user
 }
 
 func GetForm[T any](r *http.Request) *T {
